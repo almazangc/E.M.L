724e90048cab0fcd19e8bfbd013daf6acb4fc8a3
Gilbert Almazan <59057047+DOMO-Dom@users.noreply.github.com>
2022-01-25 10:40:17 +0800

    Connected Inventory Database

--- src/resources/controllers/dashboardTab/Inventory.java
+++ src/resources/controllers/dashboardTab/Inventory.java
@@ -1,8 +1,77 @@
 package resources.controllers.dashboardTab;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import resources.database.DatabaseConnection;
+import resources.database.table.FoodMenuModel;
+import resources.database.table.InventoryModel;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
 /**
  * Manages items and food raw stock in inventory
  */
-public class Inventory {
+public class Inventory implements Initializable {
+    @FXML
+    private TableView<InventoryModel> table;
 
+    @FXML
+    private TableColumn<InventoryModel, String> amount, cost;
+
+    @FXML
+    private TableColumn<InventoryModel, String> description, name, type;
+
+    private ObservableList<InventoryModel> observableList = FXCollections.observableArrayList();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Connection connection = DatabaseConnection.getInstance().getConnection();
+        Statement statement = null;
+        ResultSet resultSet = null;
+        String sql_validate = "SELECT * from inventory";
+
+        try {
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(sql_validate);
+            while (resultSet.next()){
+                InventoryModel modelTable = new InventoryModel(
+                        resultSet.getDouble("amount"),
+                        resultSet.getDouble("cost"),
+                        resultSet.getString("name"),
+                        resultSet.getString("type"),
+                        resultSet.getString("description"));
+
+                this.observableList.add(modelTable);
+            }
+        } catch (SQLException throwables) {
+            //throwables.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e){
+                System.out.println(e);
+            }
+        }
+
+        setCellValue();
+        this.table.setItems(observableList);
+    }
+
+    private void setCellValue() {
+        this.name.setCellValueFactory(new PropertyValueFactory<>("amount"));
+        this.type.setCellValueFactory(new PropertyValueFactory<>("cost"));
+        this.amount.setCellValueFactory(new PropertyValueFactory<>("name"));
+        this.cost.setCellValueFactory(new PropertyValueFactory<>("type"));
+        this.description.setCellValueFactory(new PropertyValueFactory<>("description"));
+    }
 }
--- src/resources/database/table/InventoryModel.java
+++ src/resources/database/table/InventoryModel.java
@@ -1,14 +1,54 @@
 package resources.database.table;
 
-import javafx.fxml.Initializable;
+public class InventoryModel {
+    private double amount, cost;
+    private String name, type, description;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+    public InventoryModel(double amount, double cost, String name, String type, String description) {
+        this.amount = amount;
+        this.cost = cost;
+        this.name = name;
+        this.type = type;
+        this.description = description;
+    }
 
-public class InventoryModel implements Initializable {
+    public double getAmount() {
+        return amount;
+    }
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
 
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
